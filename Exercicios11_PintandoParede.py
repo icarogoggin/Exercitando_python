9f859c4d24adeac3dd94219a8ff5c2084e4941b9
icarogoggin <icaro.goggin@gmail.com>
2021-06-02 18:35:36 -0300

    Create Exercicios11_PintandoParede.py

---
+++ Exercicios11_PintandoParede.py
@@ -0,0 +1,6 @@
+largura = float(input('Digite a largura da parede: '))
+altura = float(input('Digite a Altura da parede: '))
+area = largura*altura
+print(f'Sua parede tem a dimensão de {largura}x{altura} e sua área é de {area}m')
+tinta = area/2
+print(f'Para pintar essa parede, você precisará de {tinta}l de tinta')
