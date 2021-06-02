c9959abe35da542b2b6b1240a9f8e2ede2a5129e
icarogoggin <icaro.goggin@gmail.com>
2021-06-02 16:30:33 -0300

    Create Exercicios07_MediaAritimetica.py

---
+++ Exercicios07_MediaAritimetica.py
@@ -0,0 +1,4 @@
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: '))
+media = nota1/nota2
+print(f'A primeira nota do aluno foi {nota1} a segunda nota foi {nota2} e a média final foi {media:.2f}.')
