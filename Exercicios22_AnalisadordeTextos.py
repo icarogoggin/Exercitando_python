41be87246793a5178a66dd666b02df0d0eae42e3
icarogoggin <icaro.goggin@gmail.com>
2021-06-04 12:47:27 -0300

    Create Exercicios22_AnalisadordeTextos.py

---
+++ Exercicios22_AnalisadordeTextos.py
@@ -0,0 +1,6 @@
+nome = input('Digite seu nome completo: ').strip()
+print(f'Seu nome em maiúsculo é :{nome.upper()}')
+print(f'Seu nome em minusculo é :{nome.lower()}')
+print(f"Seu nome em minusculo é :{len(nome)-nome.count(' ')}")
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e ele tem {len(separa[0])} letras.')
